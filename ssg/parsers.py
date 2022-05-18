d92f5db7778041042005f3630eabbae8d28426ea
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 18:45:34 +0100

    6

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -15,4 +15,5 @@ class Parser:
 
     def write(self, path, dest, content, ext = ".html"):
         full_path = dest / path.with_suffix(ext).name
-        with open(full_path) as file
+        with open(full_path, "w") as file:
+            file.write(content)
