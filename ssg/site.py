a8009a9a3016579eaec8ec53d56cade442fbe887
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 19:01:32 +0100

    12

--- ssg/site.py
+++ ssg/site.py
@@ -20,3 +20,8 @@ class Site:
         for parser in self.parsers:
             if parser.valid_extension(extension)
             return parser
+
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension)
+            return parser
