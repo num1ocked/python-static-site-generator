e649e272a975948d88faa3ff888d4c5bbff08fb1
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 19:11:18 +0100

    13 missing syntax

--- ssg/site.py
+++ ssg/site.py
@@ -25,5 +25,5 @@ class Site:
         parser = self.load_parser(path.suffix)
         if parser is not None:
             parser.parse(path, self.source, self.dest)
-        else
+        else:
             print("Not Implemented")
