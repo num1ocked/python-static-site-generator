df33d36186d246465f2640936ea6470afe306c58
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-18 18:20:47 +0100

    3 annotation

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,5 +6,5 @@ class Parser:
     def valid_extension(self, extension):
         return extension in self.extensions
 
-    def parse(path, source, dest):
+    def parse(path:Path, source:Path, dest:Path):
         raise NotImplementedError
