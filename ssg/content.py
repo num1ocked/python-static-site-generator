63384cfce07c515a0b2f91e51c874e89246fbdee
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-19 10:09:16 +0100

    11

--- ssg/content.py
+++ ssg/content.py
@@ -38,5 +38,6 @@ class Content(Mapping):
     def __len__(self):
         return len(self.data)
 
-    def __repr__(self, data):
-        str(data)
+    def __repr__(self):
+        data = {}
+        return str(data)
