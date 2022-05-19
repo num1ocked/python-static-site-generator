9a98157bb5b5cb336442db9162b35e1635b1bd8e
num1ocked <105787413+num1ocked@users.noreply.github.com>
2022-05-19 10:27:02 +0100

    2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -30,3 +30,6 @@ class ResourceParser(Parser):
     extensions=[".jpg", ".png", ".gif", ".css", ".html"]
     def parse(self, path:Path, source:Path, dest:Path):
         self.copy(path, source, dest)
+
+class MarkdownParser(Parser):
+    extensions=[".md", ".markdown"]
